50ebfe5eb80e58675179db3b8ff6971b1bf056c4
Kuran V Kumar <kurankumar@gmail.com>
2019-10-02 23:03:53 +0000

    Done.

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -4,7 +4,7 @@ describe '#reverse_each_word' do
   let(:sentence1) { "Hello there, and how are you?" }
   it 'reverses all the words in a string without reversing the order of the words' do
     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
-  end
+  
 
   let(:sentence2) { "Hi again, just making sure it's reversed!" }
   it 'reverses all the words in another string without reversing the order of the words' do
